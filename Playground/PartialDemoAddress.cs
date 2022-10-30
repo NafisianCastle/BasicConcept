43a3a2d4b0097e645d6da10a9eea3ea1e3f7b2aa
NafisianCastle <nafis.cseaiub@gmail.com>
2022-10-30 11:05:48 +0600

    learned delegates with generics and partial class

---
+++ Playground/PartialDemoAddress.cs
@@ -0,0 +1,22 @@
+﻿namespace Playground
+{
+	partial class PartialDemo
+	{
+		private string city;
+		private string province;
+		private string country;
+
+		public string City { get => city; set => city = value; }
+
+		public string Province { get => province; set => province = value; }
+
+		public string Country { get => country; set => country = value; }
+
+		public string Address(string city, string province, string country) => city + ", " + province + ", " + country;
+
+		public string PersonalDetails(TwoParamDel<string> fullName, ThreeParamDel<string> address)
+		{
+			return fullName(firstName, lastName) + "\n" + address(city, province, country);
+		}
+	}
+}
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,17 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Playground
 {
-    internal class Program
+    delegate T ThreeParamDel<T>(T a, T b, T c);
+    delegate T TwoParamDel<T>(T x, T y);
+    public class Program
     {
+
         static void Main(string[] args)
-        {int i;
-            i= 1;
-            
+        {
+            PartialDemo pd = new PartialDemo
+            {
+                FirstName = "Tanveer",
+                LastName = "Zaman",
+                City = "Dhaka",
+                Province = "Dhaka",
+                Country = "Bangladesh"
+            };
+
+            //var fullName = pd.FullName(pd.FirstName, pd.LastName);
+            //var address = pd.Address(pd.City, pd.Province, pd.Country);
+            var msg = pd.PersonalDetails(fullName: new TwoParamDel<string>(pd.FullName), address: new ThreeParamDel<string>(pd.Address));
+
+            //Console.WriteLine(fullName);
+            //Console.WriteLine(address);
+            Console.WriteLine(msg);
+
         }
     }
 }
