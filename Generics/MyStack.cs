84d0974a4eefe2eb50c97d47ccd218e7f5aabe20
nafisur-welldev <117058517+nafisur-welldev@users.noreply.github.com>
2022-11-29 17:26:28 +0600

    ok

--- Generics/MyStack.cs
+++ Generics/MyStack.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Generics
+﻿namespace Generics
 {
-    internal class MyStack
+    public class MyStack<T>
     {
+        readonly int m_Size;
+        readonly T[] m_Items;
+
+        int m_StackPointer = 0;
+
+        public MyStack(int size)
+        {
+            m_Size = size;
+            m_Items = new T[m_Size];
+        }
+
+        public T Pop()
+        {
+            m_StackPointer--;
+            return m_Items[m_StackPointer];
+        }
+
+        public void Push(T item)
+        {
+            m_Items[m_StackPointer] = item;
+            m_StackPointer++;
+        }
     }
 }
