947359732e929108e33ddfbe72b07d2c6911692b
nafisur-welldev <117058517+nafisur-welldev@users.noreply.github.com>
2022-11-29 15:52:53 +0600

    ok

---
+++ Play/OutExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Play
+{
+    public class OutExample
+    {
+        public (bool, int) TryParse(string valueToParse)
+        {
+            var success = int.TryParse(valueToParse, out var result);
+            return (success, result);
+        }
+
+        public void ExampleOut(out int intValue)
+        {
+            intValue = 10;
+        }
+
+        public void ExampleOut(int intValue)
+        {
+            intValue = 10;
+
+        }
+    }
+}
